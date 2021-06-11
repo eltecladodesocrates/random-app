2e8b567295fcf16f817659c13e0378f64f577332
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-06-10 23:51:01 -0400

    webpack ready

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('app.js is running');
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const jsx = (
+    <div>
+        <h1>Testing</h1>
+    </div>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
+
